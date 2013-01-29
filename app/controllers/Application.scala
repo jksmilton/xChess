8f5964364f45d54e807d8cf143ec9c51ab0f4995
Jordan Milton <jksmilton@gmail.com>
2013-01-29 00:11:46 +0000

    Create User works so fuck you all

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,15 @@ package controllers
 import play.api._
 import play.api.mvc._
 import xmodels.DatabaseAccessor
+import xmodels.ChessUser
+import play.api.libs.json._
+import com.codahale.jerkson.Json._
+
 
 object Application extends Controller {
   
   def index = Action {
-    Ok("Hello World")
+    Ok("")
   }
   
   def getUser(username : String, email : String) = Action { request =>
@@ -20,20 +24,43 @@ object Application extends Controller {
       
     } else {
       
-      var resultString = ""
+      val jsonResult = generate(user)
       
-      user.friends.foreach(friend =>
-        
-        resultString += friend.name + ";"
-        
-      )
-      
-      Ok(resultString)
-      
+      Ok(jsonResult)
+     
     }
     
     
     
+  }
+  
+  def createUser = Action(parse.json){ request=>
+  
+    var username : String = null
+    var email : String = null
+    println("Incoming json: " + request.body)
+    
+      
+    username = (request.body \ "user").asOpt[String].map { name =>
+      name
+    }.get
+  
+    email = (request.body \ "email").asOpt[String].map { uEmail =>
+      uEmail                
+    }.get
+      
+    var user = new ChessUser(username, email)
+    println("User: " + user)
+    DatabaseAccessor.createUser(user)
+    
+    Ok("SUCCESS")
+    
+  }
+  
+  def getGame(gameID : Long) = Action{ request=>
+    
+    
+    Ok("")
   }
   
 }
